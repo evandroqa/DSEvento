d29bcb542f79eb33f14ae5a5d01566f995f97c06
evandroqa <evandroqa@gmail.com>
2024-12-10 13:14:31 -0300

    First entities

---
+++ src/main/java/com/devsuperior/dseventos/entities/Participante.java
@@ -0,0 +1,71 @@
+package com.devsuperior.dseventos.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_participante")
+public class Participante {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(columnDefinition = "TEXT")
+	private String nome;
+	
+	@Column(unique = true)
+	private String email;
+	
+	public Participante() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Participante other = (Participante) obj;
+		return Objects.equals(email, other.email) && Objects.equals(id, other.id);
+	}
+		
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_categoria(descricao) VALUES ('Curso');
+INSERT INTO tb_categoria(descricao) VALUES ('Oficina');
+
+INSERT INTO tb_participante(nome, email) values ('José da Silva', 'jose@gmail.com')
+INSERT INTO tb_participante(nome, email) values ('Tiago Faria', 'tiago@gmail.com')
+INSERT INTO tb_participante(nome, email) values ('Maria do Rosário', 'maria@gmail.com')
+INSERT INTO tb_participante(nome, email) values ('Teresa Silva', 'teresa@gmail.com')
