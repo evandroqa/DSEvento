eade5e785c20f6926ea3793455e4f46bf8f93060
EVANDRO QUEIROZ ARAUJO <101594488+evandroqa@users.noreply.github.com>
2024-12-10 12:09:41 -0300

    Projeto gerado no Spring Initializr

---
+++ src/main/java/com/devsuperior/DSEventos/DsEventosApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DSEventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsEventosApplication.class, args);
+	}
+
+}
